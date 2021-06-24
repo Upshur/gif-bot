2a606cf0381f7ee3bcd0f23e06cc80dbaa44ec22
Glitch (glitch-hello-node) <none>
2021-06-24 14:42:54 +0000

    🐧🐑 Checkpoint ./komutlar/twitch.js:32800226/866 ./app.js:32800226/3697 ./ayarlar.json:32800226/113

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const ayarlar = require("./ayarlar.json");
 const { Client, Util } = require("discord.js");
+const db = require("quick.db");
 const fs = require("fs");//gweep creative
 
 //gweep creative
@@ -46,3 +47,40 @@ client.on("ready", async () => {
   console.log("Aktif!")
 console.log(`${client.user.username} ismiyle bağlandım.`);
 })
+
+client.on("ready", async () => {
+  setInterval(async () => {
+    client.guilds.cache.forEach(async guild => {
+})
+  }, 5000);
+})
+
+/*
+
+const data = await db.get(`twitch.${guild.id}`) || { channel: null, name: null };
+if(!data.channel) return;
+if(!data.name) return;
+  
+fetch(`https://api.twitch.tv/helix/streams?user_login=${tag.name}`, { 
+method: "GET",
+headers: { "client-id": ayarlar.twitch_client_id, "Authorization": `Bearer ${ayarlar.twitch_token}` }
+}).then(response => response.json()
+.then(res => {
+if(!res.data.length) return;
+
+const channel = guild.channels.cache.get(data.channel);
+
+if(!channel) return;
+
+const username = res.data[0].user_name;
+const game = res.data[0].game_name;
+const title = res.data[0].title;
+const viewer_count = res.data[0].view_count;
+const thumbnail = res.data[0].thumbnail_url.replace("{width}", 500).replace("{height}", 250);
+
+const msg = new Discord.MessageEmbed().setTitle(title).setColor("#ffff");
+
+twitchs[guild.id] = true;
+return channel.send(msg);
+})
+         */
--- komutlar/twitch.js
+++ komutlar/twitch.js
@@ -2,16 +2,23 @@ const db = require("quick.db");
 const Discord = require('discord.js');
 exports.run = async (client, message, args) => { 
   if(args[0] === "ayarla") {
-    
+    let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[1]);
+    if(!channel) return message.channel.send("Twitch Bildirim kanalini belirtmelisin.");
+    db.set(`twitch.${message.guild.id}.channel`, channel.id);
+    message.channel.send(`Twitch Bildirim kanali başarıyla ayarlandı.`)
   } else if(args[0] === "kanal") {
-   
+   let arguman = args.slice(1).join(" ");
+   if(!arguman) return message.channel.send("Twitch Kanalini belirtmelisin.");
+   db.set(`twitch.${message.guild.id}.name`, arguman);
+ message.channel.send(`Twitch Kanali başarıyla ayarlandı.`);
   } else {
+    return message.channel.send("Bir arguman belirt `(kanal, ayarla)`");
 }
 }
   exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['tw','']
+    aliases: ['tw']
   }
   exports.help = {
     name: 'twitch'
