35e3f3c4ae689b314c27a2e8e8a0a611cbe77763
Glitch (glitch-hello-node) <none>
2021-08-15 12:16:25 +0000

    👔👌 Checkpoint ./ayarlar.json:26516246/118 ./komutlar/yardım.js:26516246/1309

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,8 +3,20 @@ const Discord = require('discord.js');
 const fetch = require("node-fetch");
 const ayarlar = require("../ayarlar.json");
 exports.run = async (client, message, args) => { 
-  
+  let prefix = ayarlar.prefix;
   const yardım = new Discord.MessageEmbed()
+  .setAuthor('Gweep Creative Gif Bot', client.user.avatarURL())
+  .setTitle('Yardım Menüsü')
+  .setDescription(`Aşağıdaki komutları kullanarak giflere ulaşabilirsiniz \n
+  \`${prefix}animal\` animal gif atar.
+  \`${prefix}anime\` anime gif atar.
+  \`${prefix}avatar\` sunucdaki üyelerin profil fotolarını atar.
+  \`${prefix}banner\` sunucdaki üyelerin bannerlarını atar.
+  \`${prefix}couple\` couple gif atar.
+  \`${prefix}man\` man gif atar.
+  \`${prefix}woman\` woman gif atar.
+  \`${prefix}yardım\` yardım menüsünü açar.`)
+  message.channel.send(yardım)
 }
   exports.conf = {
     enabled: true,
