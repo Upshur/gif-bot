5d657c0d8c54847c03458b5f2e1bd98f68df8ea9
Glitch (glitch-hello-node) <none>
2021-08-19 11:47:57 +0000

    🌔🚡 Checkpoint ./komutlar/avatar.js:26516246/851

--- komutlar/avatar.js
+++ komutlar/avatar.js
@@ -4,13 +4,18 @@ const fetch = require("node-fetch");
 const ayarlar = require("../ayarlar.json");
 exports.run = async (client, message, args) => { 
   
-  const kişicikabi = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author
-
-  const embed = new Discord.MessageEmbed()
+  const kişicikabi = message.mentions.users.first() || message.guild.members.cache.get(args[0])  || message.author
+try{
+   const embed = new Discord.MessageEmbed()
   .setAuthor(kişicikabi.tag ,kişicikabi.avatarURL())
-  .setImage(kişicikabi.avatarURL({ dynamic: true, format: 'gif', size: 1024 }))
+  .setImage(kişicikabi.avatarURL({ dynamic: true, size: 1024 }))
   .setFooter(message.author.tag + 'Tarafından istendi', message.author.avatarURL())
   message.channel.send(embed)
+}
+  catch{
+    message.channel.send('Bir hata oluştu');
+  }
+ 
  
 }
   exports.conf = {
